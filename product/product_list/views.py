278bb09f01ce7b9631cf7b0dda9849d6494863cd
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-05 19:59:25 -0300

    Editting of product_list views
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Dâmaso Júnio P. Brasileo <junio.pereirab@gmail.com>

--- product/product_list/views.py
+++ product/product_list/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from product_list.models import ProductList
+from product_list.serializers import ProductListSerializer
+
+from rest_framework import generics
+
+class ProductListCreate(generics.ListCreateAPIView):
+    queryset = ProductList.objects.all()
+    serializer_class = ProductListSerializer
+
 
 # Create your views here.
