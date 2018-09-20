13332285dc7a167af77be44859a244f188a07c8f
Lucas Costa <lucascostaa73@gmail.com>
2018-09-20 20:40:40 -0300

    Deleting old project files

--- product/product_list/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/product_list/', views.ProductListCreate.as_view()),
-    path('api/users/', views.UserViewSet.as_view()),
-]
