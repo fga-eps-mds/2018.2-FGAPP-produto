52c765b847bdb99828256091e404c719bd96d5fc
Andre Lucas <andrelucax@gmail.com>
2018-09-21 01:32:31 -0300

    Created method delete. Ignoring migrations on .gitignore.
    Co-authored-by: Andre Lucas de Sousa Pinto <andrelucax@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- product_microservice/products/urls.py
+++ product_microservice/products/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.urls import path
 from django.conf.urls import url
 from products import views
+from .views import ProductDelete
 
 urlpatterns = [
     url(r'^products/$', views.ProductList.as_view()),
     url(r'^products/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view()),
+    path('api/product_delete', ProductDelete),
 ]
--- product_microservice/products/views.py
+++ product_microservice/products/views.py
@@ -1,6 +1,13 @@
 from products.models import Product
 from products.serializers import ProductSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.status import (
+    HTTP_403_FORBIDDEN,
+    HTTP_200_OK,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
+from rest_framework.response import Response
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -9,3 +16,19 @@ class ProductList(generics.ListCreateAPIView):
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+@api_view(["POST"])
+def ProductDelete(request):
+    fk_vendor = request.data.get("fk_vendor")
+    try:
+        product = Product.objects.get(id=request.data.get("product_id"))
+    except:
+        return Response({'error': 'Produto nao existe.'},
+                                status=HTTP_500_INTERNAL_SERVER_ERROR)
+
+    if (int(fk_vendor) == product.fk_vendor):
+        product.delete()
+        return Response(status=HTTP_200_OK)
+
+    return Response({'error': 'Produto nao pertence a este usuario, permissao negada.'},
+                            status=HTTP_403_FORBIDDEN)
