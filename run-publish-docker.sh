5a99a81004e13099f4af679871a9bcac6000a525
Lucas Costa <lucascostaa73@gmail.com>
2018-10-01 13:26:23 -0300

    removing unecessary scripts

--- run-publish-docker.sh
+++ run-publish-docker.sh
@@ -1,16 +1,3 @@
-
-# echo ' - Docker image - '
-# echo $PREFIX
-# echo $IMAGE
-# echo ' - Github - '
-# echo $GH_NAME
-# echo $GH_EMAIL
-# echo $GH_TOKEN
-# echo ' - Docker login - '
-# echo $DC_USER
-# echo $DC_PASS
-# echo ' - - '
-
 set -e
 
 if [ -z "$PREFIX" ]; then
@@ -20,21 +7,6 @@ fi
 
 if [ -z "$IMAGE" ]; then
     echo 'IMAGE variable is not set'
-    exit 1
-fi
-
-if [ -z "$GH_NAME" ]; then
-    echo 'GH_NAME variable is not set'
-    exit 1
-fi
-
-if [ -z "$GH_EMAIL" ]; then
-    echo 'GH_EMAIL variable is not set'
-    exit 1
-fi
-
-if [ -z "$GH_TOKEN" ]; then
-    echo 'GH_TOKEN variable is not set'
     exit 1
 fi
 
@@ -51,6 +23,11 @@ fi
 version=`cat VERSION`
 echo "version: $version"
 
+if [ -z "$version" ]; then
+    echo 'version variable is not set'
+    exit 1
+fi
+
 docker build -t $PREFIX/$IMAGE:latest -f production.Dockerfile .
 
 # tag it
@@ -64,14 +41,3 @@ echo "publishing latest: ${PREFIX}/${IMAGE}:latest"
 docker push ${PREFIX}/${IMAGE}:latest
 echo "publishing version: ${PREFIX}/${IMAGE}:${version}"
 docker push ${PREFIX}/${IMAGE}:${version}
-
-
-
-# echo "GH_NAME='${GH_NAME}'" >> .envtemp
-# echo "GH_EMAIL=${GH_EMAIL}" >> .envtemp
-# echo "GH_TOKEN=${GH_TOKEN}" >> .envtemp
-# echo "version=${version}" >> .envtemp
-# echo "origin=${origin}" >> .envtemp
-
-# docker run --rm -v `pwd`:"/app" -w "/app" --env-file .envtemp python:3 bash -c "sh run-git-version-update.sh"
-# rm -rf .envtemp
