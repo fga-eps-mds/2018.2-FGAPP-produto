7a55d4c342787227969d89b45dc0e73c1815342d
Lucas Costa <lucascostaa73@gmail.com>
2018-10-09 15:53:13 -0300

    Adding password on postgres

--- product_microservice/product_microservice/settings/production.py
+++ product_microservice/product_microservice/settings/production.py
@@ -10,8 +10,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
         'HOST': os.environ.get('DB_HOST'), 
-        'PORT': os.environ.get('DB_PORT')
+        'PORT': os.environ.get('DB_PORT'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
