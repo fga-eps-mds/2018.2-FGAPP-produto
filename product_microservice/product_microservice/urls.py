ec5339bea027dffc0edc6d1e28b9d3f3dfd3e341
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-10-14 11:41:55 -0300

    Remove of unnecessary import

--- product_microservice/product_microservice/urls.py
+++ product_microservice/product_microservice/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from product_microservice import views
 from .views import status
 
 urlpatterns = [
